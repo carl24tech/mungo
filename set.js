e8552ddc2d6ec13f0f3573baa8445b9b4d900a78
ekitale <munjueko@gmail.com>
2024-08-28 18:28:03 +0300

    Update and rename settings.js to set.js

--- set.js
+++ set.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const { Sequelize } = require('sequelize');
-if (fs.existsSync('settings.env'))
-    require('dotenv').config({ path: __dirname + '/settings.env' });
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
 const path = require("path");
 const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
